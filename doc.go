31e0d7b81bf7cfeeb25c4ad0867c7649ac8209cc
Ivan De Marino <ivan.de.marino@gmail.com>
2026-02-15 16:38:32 +0000

    feat: adding `task doc.serve` to preview the package godoc site

--- doc.go
+++ doc.go
@@ -1,10 +1,10 @@
 // Package spelunk is a Golang library for extracting secrets from various sources (Kubernetes, Vault, env vars, files)
-// using a unified URI-based coordinate system (e.g.,  `k8s://ns/secret/key`).
+// using a unified URI-based coordinate system (e.g., k8s://ns/secret/key).
 // It simplifies secret management by abstracting backend complexity, providing a consistent API for "digging up"
 // configuration values in cloud-native apps.
 //
-// Its primary application (but... you do you) is **command line tools**. Users point at a secret from any _source_:
-// your tool will adapt based on the `/plugin`s installed.
+// Its primary application (but... you do you) is command line tools. Users point at a secret from any source:
+// your tool will adapt based on the plugins installed.
 //
 // With a single library, the source of secrets is flexible and adapts to the
 // environment and the situation.
--- source.go
+++ source.go
@@ -5,9 +5,10 @@ import (
 )
 
 // SecretSource is a source of secrets.
-// It has a Type(), matching a SecretCoord.Type, and
-// it can be asked to DigUp() a secret using the given SecretCoord.
 type SecretSource interface {
+	// Type returns the unique identifier for the type of SecretSource.
 	Type() string
+
+	// DigUp returns the secret pointed at by the given SecretCoord.
 	DigUp(context.Context, SecretCoord) (string, error)
 }
